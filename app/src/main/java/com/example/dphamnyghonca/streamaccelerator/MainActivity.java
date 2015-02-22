1e0a12c7050cb067773a6817c0b5ac3cc718d449
ruthgrace <ruthgracewong@gmail.com>
2015-02-21 23:31:20 -0500

    added hardware acceleration on application level and set webchromeclient in mainactivity, but sitll getting an error

--- app/src/main/java/com/example/dphamnyghonca/streamaccelerator/MainActivity.java
+++ app/src/main/java/com/example/dphamnyghonca/streamaccelerator/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.dphamnyghonca.streamaccelerator;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
-
+import android.view.Window;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -15,6 +19,29 @@ public class MainActivity extends ActionBarActivity {
         WebView webview = new WebView(this);
         setContentView(webview);
         //setContentView(R.layout.activity_main);
+
+        // Let's display the progress in the activity title bar, like the
+        // browser app does.
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
+
+        webview.getSettings().setJavaScriptEnabled(true);
+
+        final Activity activity = this;
+        webview.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                // Activities and WebViews measure progress with different scales.
+                // The progress meter will automatically disappear when we reach 100%
+                activity.setProgress(progress * 1000);
+            }
+        });
+        webview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //webview.loadUrl("http://developer.android.com/");
+
         webview.loadUrl("file:///android_asset/test.html");
         //WebSettings.setBuiltInZoomControls(true);
     }
